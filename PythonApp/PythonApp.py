39500907467b49f9d848f06878ec4f93aaed2beb
rc_luca@yahoo.com <rc_luca@yahoo.com>
2015-08-22 15:57:19 -0400

    Added parsing for email

--- PythonApp/PythonApp.py
+++ PythonApp/PythonApp.py
@@ -1,4 +1,4 @@
-import imaplib
+import imaplib, email, re
 from HTMLParser import HTMLParser
 
 class MLStripper(HTMLParser):
@@ -25,43 +25,60 @@ imap = imaplib.IMAP4_SSL(imap_host)
 ## login
 imap.login(imap_user, imap_pass)
 
-## get status for the mailbox (folder) INBOX
-folderStatus, UnseenInfo = imap.status('INBOX', "(UNSEEN)")
-
-print folderStatus
-
-NotReadCounter = int(UnseenInfo[0].split()[2].strip(').,]'))
-print NotReadCounter
-
-### create a new folder
-#status, createFolder_response = imap.create('myFolders.xyz')
-
-### folders list
-#status, folder_list = imap.list()
-
-### list sub-folders
-#status, sub_folder_list = imap.list(directory='insd')
-
 ## select a specific folder
 status, data = imap.select('Flight Deal')
 
-### searching current folder using title keywords 
-#status, messages = imap.search(None, '(SUBJECT "Work Report")')
- 
-### fetching message header by  using message( ID)
-#status, msg_header = imap.fetch('1', '(BODY.PEEK[HEADER])')
-
-### fetching the full message ( ID=1)
-#status, AllTheMessage= imap.fetch('1', '(RFC822)')
-
-### moving/copying messages around folders
-#status, messages  = imap.copy(msg_ids, 'myFolders.xyz')
-#status, messages  = imap.move(msg_ids, 'otherFolder')
-
+## retrieve and print all messages
+msg = ""
 typ, data = imap.search(None, 'ALL')
 for num in data[0].split():
-    typ, data = imap.fetch(num, '(RFC822)')
-    print 'Message %s\n%s\n' % (num, strip_tags(data[0][1]))
 
+    # get message
+    typ, data = imap.fetch(num, '(RFC822)')
+    
+    # parse message into required info
+    msg = data[0][1]
+    match = re.search(r"You are receiving this email because you elected to receive emails from", msg)
+    msg = msg[match.regs[0][1]:]
+    match = re.search(r"http://theflightdeal.us3.list-manage1.com", msg)
+    msg = msg[:match.regs[0][0]]
+    msg = msg.replace("\r\n", "").replace("=2C", ", ")
+    
+    msgList = msg.split("*")
+    
+    for m in msgList[2:]:
+        subMsg1 = m.split("=E2=80=93")
+        for sm1 in subMsg1:
+            sm1 = sm1.replace("=", "")
+            print sm1
+    
 imap.close()
 imap.logout()
+
+
+# Th=
+#e Flight Deal
+#Deals for 07/02/2015:
+#* Save the Date: The Flight Deal Team NYC Meet & Greet =E2=80=93 Tuesday=
+#=2C August 4th=2C 2015
+#* Air France =E2=80=93 $742: Phoenix / Dallas / Miami / Philadelphia / Cha=
+#rlotte / St. Louis =E2=80=93 Skopje=2C Macedonia. Roundtrip=2C including a=
+#ll Taxes
+#* Air Canada =E2=80=93 $715: San Francisco =E2=80=93 Seoul=2C South Korea.=
+# Roundtrip=2C including all Taxes
+#* American =E2=80=93 $225: Philadelphia =E2=80=93 Salt Lake City=2C Utah (=
+#and vice versa). Roundtrip=2C including all Taxes
+#* Aeromexico =E2=80=93 $318: Boston =E2=80=93 Cabo San Lucas=2C Mexico. Ro=
+#undtrip=2C including all Taxes
+#* American =E2=80=93 $812: Los Angeles =E2=80=93 Barcelona=2C Spain. Round=
+#trip=2C including all Taxes
+#* The Shorthaul =E2=80=93 United =E2=80=93 $146: Chicago =E2=80=93 Fort La=
+#uderdale (and vice versa). Roundtrip=2C including all Taxes
+#* United =E2=80=93 $821: Newark =E2=80=93 Jakarta=2C Indonesia. Roundtrip=
+#=2C including all Taxes
+#* KLM =E2=80=93 $577: Washington D.C. =E2=80=93 St. Petersburg=2C Russia.=
+# Roundtrip=2C including all Taxes
+#* Copa =E2=80=93 $202: Washington D.C.  =E2=80=93 San Salvador=2C El Salva=
+#dor. Roundtrip=2C including all Taxes
+#* [FARE GONE] Alaska =E2=80=93 $196: Baltimore =E2=80=93 Los Angeles (and=
+# vice versa). Roundtrip=2C including all Taxes
