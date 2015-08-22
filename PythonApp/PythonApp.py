59f29f7c0d5fe868e435e1a44747117eacef5c7f
rc_luca@yahoo.com <rc_luca@yahoo.com>
2015-08-22 15:57:56 -0400

    Removed a web address appearing in certain locations

--- PythonApp/PythonApp.py
+++ PythonApp/PythonApp.py
@@ -17,7 +17,7 @@ def strip_tags(html):
 
 imap_host = "imap.mail.yahoo.com"
 imap_user = "rc_luca@yahoo.com"
-imap_pass = input()
+imap_pass = input("Please enter password: ")
 
 ## open a connection 
 imap = imaplib.IMAP4_SSL(imap_host)
@@ -39,14 +39,17 @@ for num in data[0].split():
     # parse message into required info
     msg = data[0][1]
     match = re.search(r"You are receiving this email because you elected to receive emails from", msg)
-    msg = msg[match.regs[0][1]:]
+    if (match != None):
+        msg = msg[match.regs[0][1]:]
     match = re.search(r"http://theflightdeal.us3.list-manage1.com", msg)
-    msg = msg[:match.regs[0][0]]
+    if (match != None):
+        msg = msg[:match.regs[0][0]]
     msg = msg.replace("\r\n", "").replace("=2C", ", ")
+    msg = re.sub("http:.* ","",msg)
     
     msgList = msg.split("*")
     
-    for m in msgList[2:]:
+    for m in msgList:
         subMsg1 = m.split("=E2=80=93")
         for sm1 in subMsg1:
             sm1 = sm1.replace("=", "")
@@ -54,31 +57,3 @@ for num in data[0].split():
     
 imap.close()
 imap.logout()
-
-
-# Th=
-#e Flight Deal
-#Deals for 07/02/2015:
-#* Save the Date: The Flight Deal Team NYC Meet & Greet =E2=80=93 Tuesday=
-#=2C August 4th=2C 2015
-#* Air France =E2=80=93 $742: Phoenix / Dallas / Miami / Philadelphia / Cha=
-#rlotte / St. Louis =E2=80=93 Skopje=2C Macedonia. Roundtrip=2C including a=
-#ll Taxes
-#* Air Canada =E2=80=93 $715: San Francisco =E2=80=93 Seoul=2C South Korea.=
-# Roundtrip=2C including all Taxes
-#* American =E2=80=93 $225: Philadelphia =E2=80=93 Salt Lake City=2C Utah (=
-#and vice versa). Roundtrip=2C including all Taxes
-#* Aeromexico =E2=80=93 $318: Boston =E2=80=93 Cabo San Lucas=2C Mexico. Ro=
-#undtrip=2C including all Taxes
-#* American =E2=80=93 $812: Los Angeles =E2=80=93 Barcelona=2C Spain. Round=
-#trip=2C including all Taxes
-#* The Shorthaul =E2=80=93 United =E2=80=93 $146: Chicago =E2=80=93 Fort La=
-#uderdale (and vice versa). Roundtrip=2C including all Taxes
-#* United =E2=80=93 $821: Newark =E2=80=93 Jakarta=2C Indonesia. Roundtrip=
-#=2C including all Taxes
-#* KLM =E2=80=93 $577: Washington D.C. =E2=80=93 St. Petersburg=2C Russia.=
-# Roundtrip=2C including all Taxes
-#* Copa =E2=80=93 $202: Washington D.C.  =E2=80=93 San Salvador=2C El Salva=
-#dor. Roundtrip=2C including all Taxes
-#* [FARE GONE] Alaska =E2=80=93 $196: Baltimore =E2=80=93 Los Angeles (and=
-# vice versa). Roundtrip=2C including all Taxes
